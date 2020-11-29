45b401d76fcf83843fb4a2297dfb978d77b241e7
Quimi <david.crespo@correounivalle.edu.co>
2020-11-29 00:14:47 -0500

    Primeros pasos

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'America Precolombina',
+      title: 'América Precolombina',
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: getAppRoutes(),
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,12 +5,15 @@ import 'package:america_precolombina/src/pages/home_page.dart';
 import 'package:america_precolombina/src/pages/incas_page.dart';
 import 'package:america_precolombina/src/pages/mayas_page.dart';
 import 'package:america_precolombina/src/pages/aztecas_page.dart';
+import 'package:america_precolombina/src/pages/incas/ubicacion.dart';
 
 Map<String, WidgetBuilder> getAppRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => HomePage(),
     'incas': (BuildContext context) => IncasPage(),
     'mayas': (BuildContext context) => MayasPage(),
-    'aztecas': (BuildContext context) => AztecasPage()
+    'aztecas': (BuildContext context) => AztecasPage(),
+
+    'incas/ubicacion': (BuildContext context) => IncasUbicacionPage(),
   };
 }
