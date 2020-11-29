e25f8e13b346e24d9fc4b707ad2b19ef86524584
henrysala <henry.salazar@correounivalle.edu.co>
2020-11-28 21:12:18 -0500

    cambie los iconos de las paginas en el home por CircleAvatar

--- lib/src/utils/icons_string_utils.dart
+++ lib/src/utils/icons_string_utils.dart
@@ -7,9 +7,28 @@ final _icons = <String, IconData>{
   'map': Icons.map
 };
 
+final _symbols = <String, CircleAvatar>{
+  'incas': CircleAvatar(
+    backgroundImage: NetworkImage(
+        'https://www.simboloteca.com/wp-content/uploads/2019/04/viracocha.jpg'),
+  ),
+  'mayas': CircleAvatar(
+    backgroundImage: NetworkImage(
+        'https://grandesmitos.com/wp-content/uploads/2020/03/simbolog%C3%ADa-maya1.jpg'),
+  ),
+  'aztecas': CircleAvatar(
+    backgroundImage: NetworkImage(
+        'https://diccionariodesimbolos.org/wp-content/uploads/2019/11/bf27726b6a5ad140664b6550ac3e04a7-300x264.jpg'),
+  ),
+};
+
 Icon getIcon(String nameIcon) {
   return Icon(
     _icons[nameIcon],
     color: Colors.amber,
   );
+}
+
+CircleAvatar getSymbol(String nameSymbol) {
+  return _symbols[nameSymbol];
 }
