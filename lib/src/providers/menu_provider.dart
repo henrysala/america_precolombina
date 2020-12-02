7a079a3a9296174e38854f26817b28d384527d66
henrysala <henry.salazar@correounivalle.edu.co>
2020-12-01 22:23:37 -0500

    cree un archivo json con la información de la pagina ubicacion aztecas y modifique el codigo para que la informción se obtenga del json y no se escriba directamente en el codigo, como lo sugirio la profesora

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 
-class _MenuProvider {
+class _MenuProviderHome {
   List<dynamic> options = [];
 
   Future<List<dynamic>> loadData() async {
@@ -19,14 +19,28 @@ class _MenuProviderAztecas {
   List<dynamic> options = [];
 
   Future<List<dynamic>> loadData() async {
-    final resp = await rootBundle.loadString('data/menu_options.json');
+    final resp = await rootBundle.loadString('data/aztecas_info.json');
 
     Map dataJson = json.decode(resp);
-    options = dataJson['carpetasAztecas'];
+    options = dataJson['rutasAztecas'];
 
     return options;
   }
 }
 
-final menuProvider = new _MenuProvider();
+class _CardProviderUbicacionAztecas {
+  List<dynamic> info = [];
+
+  Future<List<dynamic>> loadData() async {
+    final resp = await rootBundle.loadString('data/aztecas_info.json');
+
+    Map dataJson = json.decode(resp);
+    info = dataJson['ubicacion'];
+
+    return info;
+  }
+}
+
+final menuProviderHome = new _MenuProviderHome();
 final menuProviderAztecas = new _MenuProviderAztecas();
+final cardProviderUbicacionAztecas = new _CardProviderUbicacionAztecas();
